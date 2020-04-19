ac6218f33bf22cbc8fc1a0e6f74104eb7ab7ee3b
lyy <liuyueyang56@163.com>
2020-04-19 11:11:54 +0800

    更新了代码

--- git_test/src/com/itheima/testgit.java
+++ git_test/src/com/itheima/testgit.java
@@ -8,10 +8,17 @@ public class testgit {
         System.out.println("test git");
             System.out.println("test git");
     //hello test
+                //方法
+                   git();
+
     }
     public void test(){
         int i=0;
         i +=10;
         System.out.println(i);
     }
+            //添加了git方法来优化代码，比原来传的代码好多了 ，你好好学习一下
+                public static void git(){
+            System.out.println("这是我的git");
+        }
 }
